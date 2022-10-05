d1807d8b6400e8e834a98ea9d29898b9e54fad86
fikriibrahimahid <fikriibrahim1999@gmail.com>
2022-10-05 09:25:54 +0700

    -

--- lead/lead.go
+++ lead/lead.go
@@ -1,1 +1,30 @@
 package lead
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+)
+
+type Lead struct {
+	gorm.Model
+	Name    string
+	Company string
+	Email   string
+	Phone   int
+}
+
+func GetLeads(c *fiber.Ctx) {
+
+}
+
+func GetLead(c *fiber.Ctx) {
+
+}
+
+func NewLead(c *fiber.Ctx) {
+
+}
+
+func DeleteLead(c *fiber.Ctx) {
+
+}
